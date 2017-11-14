5d0e646497d7f501f858b92b6de1a798fdd01b6b
Riley Boyd <contact@rileyboyd.com>
2017-11-14 15:07:20 -0500

    Creating react app using create-react-app- npm module. Taking code from index.html and abstracting it out to the new structure

---
+++ bulletin-board/src/Board.js
@@ -0,0 +1,91 @@
+import React from 'react'
+import './App.css'
+import Note from './Note'
+
+var Board = React.createClass({
+  // Checks to see if the value of count is a number.
+  propTypes: {
+    count: function(props,propName){
+      if(typeof props[propName] !== "number") {
+        return new Error("The count must be a number");
+      }
+
+      // Checks to see if the value of propName ('count', in this case) is greater than 100
+      if(props[propName] > 100) {
+        return new Error("Creating " + props[propName] + " notes is ridiculous.")
+      }
+
+    }
+  },
+
+  getInitialState() {
+    return {
+      notes: []
+    }
+  },
+
+  componentWillMount() {
+    if(this.props.count) {
+      var url = `https://baconipsum.com/api/?type=all-meat&sentences=${this.props.count}`
+      fetch(url)
+        .then(results => results.json())
+        .then(array => array[0])
+        .then(text => text.split('. '))
+        .then(array => array.forEach(
+          sentence => this.add(sentence)
+        ))
+        .catch(function(err) {
+          console.log("Didn't connect to the API", err)
+        })
+    }
+  },
+
+  // Generates new ID
+  nextId() {
+    this.uniqueId = this.uniqueId || 0
+    return this.uniqueId++
+  },
+
+  add(text) {
+    var notes = [
+      ...this.state.notes,
+      {
+        id: this.nextId(),
+        note: text
+      }
+    ];
+    this.setState({notes});
+  },
+
+  update(newText,id) {
+    var notes = this.state.notes.map(
+      // Huh, guess this spread operator is part of ES6
+      note => (note.id !== id) ? note : {...note, note: newText}
+    );
+    this.setState({notes});
+  },
+
+  remove(id) {
+    var notes = this.state.notes.filter(note => note.id !== id);
+    this.setState({notes});
+  },
+
+  eachNote(note) {
+    return (<Note key={note.id} 
+        id={note.id} 
+        onChange={this.update} 
+        onRemove={this.remove}>
+      {note.note}
+    </Note>)
+  },
+
+  render() {
+    return (<div className='board'>
+        {this.state.notes.map(this.eachNote)}
+        <button onClick={() => this.add("New note")}>+</button>
+      </div>)
+  }
+});
+
+
+export default Board
---
+++ bulletin-board/src/Note.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import './App.css'
+
+var Note = React.createClass({
+	getInitialState() {
+		return {editing: false}
+	},
+	// componentWillMount method runs right before elements are rendered
+	componentWillMount(){
+		this.style = {
+			right: this.randomBetween(0, window.innerWidth - 150, 'px'),
+			top: this.randomBetween(0, window.innerHeight - 150, 'px' )
+		}
+	},
+	componentDidUpdate() {
+		if (this.state.editing) {
+			this.refs.newText.focus()
+			this.refs.newText.select()
+		}
+	},
+	shouldComponentUpdate(nextProps, nextState) {
+		return this.props.children !== nextProps.children || this.state !== nextState
+	},
+	edit(){
+		this.setState({editing: true});
+	},
+	randomBetween(x,y,s) {
+		return (x + Math.ceil(Math.random() * (y-x))) + s
+	},
+	save(){
+		this.props.onChange(this.refs.newText.value, this.props.id);
+		this.setState({editing:false});
+	},
+	remove(){
+		this.props.onRemove(this.props.id);
+	},
+	renderForm(){
+		return (
+			<div className="note"
+				style={this.style}>
+				<textarea ref="newText" defaultValue={this.props.children}></textarea>
+				<button onClick={this.save}>Save</button>
+			</div>
+		)
+	},
+	renderDisplay(){
+		return (
+			<div className="note"
+				style={this.style}>
+				<p>{this.props.children}</p>
+				<span>
+					<button onClick={this.edit}>Edit</button>
+					<button onClick={this.remove}>X</button>
+				</span>
+			</div>
+		)
+	},
+	render() {
+		return (<ReactDraggable>
+			{(this.state.editing) ? this.renderForm() : this.renderDisplay()}
+		</ReactDraggable>
+		)
+	}
+
+});
+
+export default Note
