80e0fbbcb184113a5edc3b6dba75c57590e2d8b2
Riley Boyd <contact@rileyboyd.com>
2017-11-16 14:19:54 -0500

    Adding an array with objects for Todo items in it

--- react-development-starter-guide-tutorial/src/App.js
+++ react-development-starter-guide-tutorial/src/App.js
@@ -2,6 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var toDos = [
+  // Array with list of ToDo objects in it.
+  {
+    toDoTitle: 'My First ToDo',
+    toDoDescription: 'Sebastian',
+    toDoResponsible:'My first Todo description',
+    todoPriority: 'low'
+  },
+  {
+    toDoTitle: 'My Second ToDo',
+    toDoDescription: 'Sebastian',
+    toDoResponsible:'My Second Todo description',
+    todoPriority: 'medium'
+  },
+  {
+    toDoTitle: 'My Third ToDo',
+    toDoDescription: 'Sebastian',
+    toDoResponsible:'My Thid Todo description',
+    todoPriority: 'high'
+  }
+]
+
 class App extends Component {
   render() {
     return (
