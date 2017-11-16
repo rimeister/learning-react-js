160b58cc38b46f4846f7ba25bd036688cc639eb6
Riley Boyd <contact@rileyboyd.com>
2017-11-16 17:50:18 -0500

    I added my own component that says 'Nice one, bud', when it is clicked. Used ternary operator and setState.

---
+++ react-development-starter-guide-tutorial/src/NiceOneBud.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+class NiceOneBud extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isNice: false
+      //
+    }
+  }
+
+  setTheNiceOne() {
+    this.setState ({
+      isNice: true
+    })
+  }
+
+  render() {
+    return (
+        <div className="niceDiv btn btn-danger btn-md" onClick={this.setTheNiceOne.bind(this)}>{(this.state.isNice) ? 'Nice one,' : null} Bud</div>
+    )
+  }
+
+}
+
+export default NiceOneBud;
--- react-development-starter-guide-tutorial/src/index.js
+++ react-development-starter-guide-tutorial/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import NiceOneBud from './NiceOneBud';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<NiceOneBud />, document.getElementById('root'));
+
 registerServiceWorker();
