0fc85450802a1aeaf7b840e7b3f617c45496f334
Riley Boyd <contact@rileyboyd.com>
2017-11-16 13:15:05 -0500

    Removing deleted files

--- bulletin-board/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
-import Container from './Container'
-import './index.css'
-
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Container />, document.getElementById('root'));
-registerServiceWorker();
